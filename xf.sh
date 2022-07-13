45dbf61be6bd6346d64607493eefc2f02ce62579
poloseaman <65015295+poloseaman@users.noreply.github.com>
2022-07-13 14:28:22 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "e2f075e2-7e2f-48ee-828f-540f1d75e96c",
                         "alterId": 0
                     }
                 ],
